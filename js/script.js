e939915080a98f0a37a84ab03e8e27dbb38eae01
Maximiliano Carabes <carabesmaximiliano@gmail.com>
2023-07-20 07:57:51 -0300

    segunda preentrega js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,82 @@
-let carrito = "";
+
+let carrito = [];
 let nuevaOperacion = false;
 
-let item = prompt("Ingrese el nombre del item que desea comprar (Mountain Bike, Electric Bike, Racing Bike, Daily Use, Bmx");
-let cantidad = Number(prompt(`Ingrese la cantidad de ${item} que desea comprar:`))
+const productos = [
+    {
+        nombre: "Mountain Bike",
+        precio: 115000,
+    },
+    {
+        nombre: "Electric Bike",
+        precio: 260000,
+    },
+    {
+        nombre: "Racing Bike",
+        precio: 1400000,
+    },
+    {
+        nombre: "Daily Use",
+        precio: 80000,
+    },
+    {
+        nombre: "BMX",
+        precio: 110000,
+    },
+];
 
-function hacerCarrito (item, cantidad) {
+function hacerCarrito() {
     do {
-        seleccionarItem(item, cantidad)
-            nuevaOperacion = confirm("¿Desea agregar otro item al carrito?")
+        const item = prompt(
+            "Ingrese el nombre del item que desea comprar:\n(Mountain Bike, Electric Bike, Racing Bike, Daily Use, BMX)"
+        );
 
-            if (nuevaOperacion) {
-                item = prompt("Ingrese el nombre del producto que desea comprar (Mountain Bike, Electric Bike, Racing Bike, Daily Use, Bmx");
-                cantidad = parseInt(prompt(`Ingrese la cantidad de ${item} que desea comprar:`));
-            }
         
-    } while(nuevaOperacion);
+        const nombreProducto = item.toUpperCase();
+        const cantidad = parseInt(prompt(`Ingrese la cantidad de ${item} que desea comprar:`), 10);
 
-    console.log("Items en el carrito:");
-    console.log(carrito);
-}
+        const productoEncontrado = buscarProducto(nombreProducto);
 
-function seleccionarItem(item, cantidad){
-
-    switch (item.toUpperCase()) {
-        case "MOUNTAIN BIKE":
-            carrito += `Mountain Bike + Cantidad: ${cantidad} + Precio Total: ${115000 * cantidad} pesos\n`;
-            break;
-        case "ELECTRIC BIKE":
-            carrito += `Electric Bike + Cantidad: ${cantidad} + Precio Total: ${260000 * cantidad} pesos\n`;
-            break;
-        case "RACING BIKE":
-            carrito += `Racing Bike + Cantidad: ${cantidad} + Precio Total: ${1400000 * cantidad} pesos\n`;
-            break;
-        case "DAILY USE":
-            carrito += `Daily Use + Cantidad: ${cantidad} + Precio Total: ${80000 * cantidad} pesos\n`;
-            break;
-            case "BMX":
-            carrito += `Bmx + Cantidad: ${cantidad} + Precio Total: ${110000 * cantidad} pesos\n`;
-            break;
-        default:
+        if (productoEncontrado) {
+            carrito.push({
+                nombre: productoEncontrado.nombre,
+                cantidad: cantidad,
+                precioTotal: productoEncontrado.precio * cantidad,
+            });
+        } else {
             alert("El producto ingresado no está disponible.");
-            break;
-    }
+        }
+
+        nuevaOperacion = confirm("¿Desea agregar otro item al carrito?");
+    } while (nuevaOperacion);
+
+    console.log("Items en el carrito: ");
+    console.log(carrito);
+
+    mostrarPrecioPorItem();
+    calcularTotalCompra();
 }
 
-hacerCarrito(item, cantidad);
+function buscarProducto(nombre) {
+    return productos.find((producto) => producto.nombre.toUpperCase() === nombre);
+}
+
+function mostrarPrecioPorItem() {
+    console.log("Precio por item:");
+    carrito.forEach((item) => {
+        console.log(`${item.nombre}: ${item.precioTotal}`);
+    });
+}
+
+function calcularTotalCompra() {
+    let totalCompra = 0;
+    carrito.forEach((item) => {
+        totalCompra += item.precioTotal;
+    });
+    console.log("Total de la compra:", totalCompra);
+}
+
+hacerCarrito();
+
+
 
